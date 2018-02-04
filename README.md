9df9b4c0291aec002d6b767ae7d182ac6761d2f9
David James <davidj28827@gmail.com>
2018-02-04 14:25:49 +1100

    Preparing for prod 📦

--- README.md
+++ README.md
@@ -2,20 +2,24 @@
 
 Small NodeJS app that automatically emails a file from a given Dropbox folder.
 
-Uses the [Dropbox JS SDK](https://github.com/dropbox/dropbox-sdk-js) and
-[Nodemailer](https://github.com/nodemailer/nodemailer)
-
-## What's left
-- [ ] Cron Job
-- [x] Logic for determing the reciever of the PDF
-- [x] Improve file find method using Dropbox API
-- [x] Archiving file / renaming after email is sent
+## Uses
+- [Dropbox JS SDK](https://github.com/dropbox/dropbox-sdk-js)
+- [Nodemailer](https://github.com/nodemailer/nodemailer)
+- [node-cron](https://github.com/kelektiv/node-cron)
 
 ## How It Works 🔧
-// TODO
+The application was made for a very specific use case, however the general problem it resolves is to automate sending an invoice to a client.
 
-## Build & Deploy :alien:
-// TODO
+This invoice is usually handwritten, scanned and uploaded to Dropbox. In the past, this would be then manually sent via email.
+
+This application rectifies this by checking if the file has been uploaded to Dropbox and proceeds to generate the email and send it to the specific client as an email with an attachment, based on the uploaded file name prefix.
+
+## Build & Usage :alien:
+1. Clone repo
+1. Set environment variables in ```variables.env```
+1. Set at least one ```person.json``` in ```data``` folder
+1. ```npm install```
+1. ```npm start```
 
 ## Helpful Links
 - [Dropbox API Docs](http://dropbox.github.io/dropbox-sdk-js/)
--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const attachedFile = require('./helpers/attached-file');
 const mail = require('./helpers/mail');
 const recipients = require('./helpers/recipients');
 
+const CronJob = require('cron').CronJob;
+
+
 // Create our Express app
 const app = express();
 
@@ -10,47 +13,61 @@ app.get('/', (req, res) => {
   res.send('Tap for mana!');
 });
 
-const recipientList = recipients.getRecipientFiles('./data');
-
-recipientList.forEach(personFile => {
-  if (personFile.includes('sample')) {
-    return;
-  }
-  const recipient = require(personFile);
-  const filePrefix = recipient['file-prefix'];
-  let foundFilePath;
-
-  // Get the file, email it then archive it!
-  const sendInvoice = attachedFile.searchFilePath(filePrefix)
-    .then(function (filePath){
-      foundFilePath = filePath;
-      const file = attachedFile.getFile(filePath);
-      return file;
-    })
-    .then(function (file) {
-      const mailPromise = mail.send({
-        email: recipient.email,
-        subject: recipient.subject,
-        text: recipient.text,
-        attachments: {   // binary buffer as an attachment
-          filename: file.name,
-          content: new Buffer(file.fileBinary, 'binary'),
-          encoding: 'binary'
-        }
-      });
-      return mailPromise;
-    })
-    .then(function () {
-      console.log('The email was sent! 📤');
-      const archiveFile = attachedFile.archiveFile(foundFilePath, recipient.name);
-      return archiveFile;
-    })
-    .catch(function (error) {
-      console.log(error);
-      console.log('Email failed to send 🙃');
-      return Promise.reject(error);
-    });
+// Start the Cron
+const job = new CronJob({
+  cronTime: '* 0 * * * *', // Once every hour
+  onTick: function(){
+    console.log('🏄  Surfing the net for the invoice');
+    sendInvoice();
+  },
+  start: false,
+  timeZone: 'Australia/Melbourne'
 });
+job.start();
+
+function sendInvoice() {
+  const recipientList = recipients.getRecipientFiles('./data');
+  recipientList.forEach(personFile => {
+    if (personFile.includes('sample')) {
+      return;
+    }
+    const recipient = require(personFile);
+    const filePrefix = recipient['file-prefix'];
+    let foundFilePath;
+
+    // Get the file, email it then archive it!
+    const sendInvoice = attachedFile.searchFilePath(filePrefix)
+      .then(function (filePath){
+        foundFilePath = filePath;
+        const file = attachedFile.getFile(filePath);
+        return file;
+      })
+      .then(function (file) {
+        const mailPromise = mail.send({
+          email: recipient.email,
+          subject: recipient.subject,
+          text: recipient.text,
+          attachments: {   // binary buffer as an attachment
+            filename: file.name,
+            content: new Buffer(file.fileBinary, 'binary'),
+            encoding: 'binary'
+          }
+        });
+        return mailPromise;
+      })
+      .then(function () {
+        console.log('The email was sent! 📤');
+        const archiveFile = attachedFile.archiveFile(foundFilePath, recipient.name);
+        return archiveFile;
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log('Email failed to send 🙃');
+        return Promise.reject(error);
+      });
+  });
+}
+
 
 // Export it so we can start the site in start.js
 module.exports = app;
