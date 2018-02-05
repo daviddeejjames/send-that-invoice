bc5330bf3e32774cbc6d7bddc2c2ccfad6579597
David James <davidj28827@gmail.com>
2018-02-05 11:21:08 +1100

    Updated travis again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,4 @@ set -x
 tar -czf package.tgz build && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
 ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+echo 'Deploy script complete 🌽'
--- scripts/untar.sh
+++ scripts/untar.sh
@@ -4,6 +4,7 @@ set -x
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v9.5.0/bin
 
+echo 'Untar script started ⭐'
 cd /opt/send-that-invoice && \
 tar zxvf package.tgz -C . && \
 mv build/package.json . && \
