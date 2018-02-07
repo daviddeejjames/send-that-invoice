ab6c90825db249ded15845b196d84b0efdb0744c
David James <david.james@chromatix.com.au>
2018-02-07 16:11:19 +1100

    Fix2.0 come on Timmy... :star:

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-tar -cvzf package.tgz --exclude=.git --exclude=package.tgz --exclude=variables.env --exclude=data .
-scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+tar -cvzf package.tgz --exclude=.git --exclude=package.tgz --exclude=variables.env --exclude=data --exclude=deploy .
+scp package.tgz "$REMOTE_USER"@"$REMOTE_HOST":"$REMOTE_APP_DIR"
+ssh "$REMOTE_USER"@"$REMOTE_HOST" 'bash -s' < ./scripts/untar.sh
 echo 'Deploy script complete 🌽'
